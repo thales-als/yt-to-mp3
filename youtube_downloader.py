dad9f6a482d79fb9466ca173e28dc0e4916ad71f
thales-als <thalessouza.dev@gmail.com>
2024-12-04 10:47:09 -0300

    first commit

---
+++ youtube_downloader.py
@@ -0,0 +1,34 @@
+import os
+import yt_dlp
+from pydub import AudioSegment
+
+if not os.path.exists("songs"):
+    os.makedirs("songs")
+
+def download_videos_from_playlist(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'songs/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',
+        }],
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+def extract_audio_from_videos():
+    for video_file in os.listdir('songs'):
+        if video_file.endswith(('.mp4', '.mkv', '.webm', '.avi')):
+            video_path = os.path.join('songs', video_file)
+            audio = AudioSegment.from_file(video_path)
+            audio_file_path = os.path.join('songs', f"{os.path.splitext(video_file)[0]}.mp3")
+            audio.export(audio_file_path, format="mp3")
+            os.remove(video_path)
+            print(f"Áudio extraído e vídeo {video_file} apagado.")
+
+playlist_url = 'https://youtube.com/playlist?list=PLKFiOX8fS7s2WNsqqqdpwcvP-BZuqqUbi&si=X1lfa4gHL6B3Z4QQ'
+
+download_videos_from_playlist(playlist_url)
+extract_audio_from_videos()
